114fe2a9e79887dc937b6df56485a51aa93e5906
Diego Pinto da Jornada <diego.p.jornada@gmail.com>
2016-06-11 00:26:37 -0300

    Updates reader handling

--- src/app.rb
+++ src/app.rb
@@ -1,45 +1,46 @@
-
 Dir['classes/*.rb'].each do |file|
 	require_relative file
 end
 
 counter = 0
-nodes   = Array.new
-routers = Array.new
+nodes   = {}
+routers = {}
+rourter_table = []
 
-File.open ARGV[0], "r" do |f|
+File.open ARGV[0], "r" do |file|
 
-	f.each_line do |line|
+	file.each_line do |line|
 		if line[0] == "#"
 			counter += 1
 			next
 		end
 
-		line = line.split ','
-		line.map! { |x| x.chomp }
+		line = line.chomp.split(',')
 
 		case counter
 		when 1 #when reading node
 			interface = Interface.new(line[1], line[2], line[3])
 			node      = Node.new(line[0], line[4], interface)
-			nodes    << node
+			nodes[line[0]] = node
 		when 2 #when reading router
 			interfaces = Array.new
 			name  = line[0]
 			ports = line[1].to_i
-			line  = line.shift 2
+			line.shift(2)
 			ports.times do |x|
 				mac, ip, mtu = line.shift 3
 				interface    = Interface.new(mac, ip, mtu)
-				interfaces  << interface
+				interfaces.push(interface)
 			end
 			router   = Router.new(name, ports, interfaces)
-			routers << router
+			routers[name] = router
 		when 3 #when reading routertable
-			puts line.to_s
+			#puts line.to_s
+			routers[line[0]].routes.push(line[1,3])
 		end
 
 	end
-
+	#puts nodes
+	puts routers
 end
 
--- src/classes/router.rb
+++ src/classes/router.rb
@@ -2,9 +2,11 @@ class Router
 	attr_reader :name
 	attr_reader :num_ports
 	attr_reader :interfaces
+	attr_accessor :routes
 	def initialize(name, num_ports, interfaces)
 		@name       = name
 		@num_ports  = num_ports
 		@interfaces = interfaces
+		@routes = []
 	end
 end
