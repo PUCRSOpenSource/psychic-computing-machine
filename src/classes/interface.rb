0d34c5c78764f715412c1ef62531db6dba8a4506
Diego Pinto da Jornada <diego.p.jornada@gmail.com>
2016-06-18 00:52:53 -0300

    Refactor interface

--- src/classes/interface.rb
+++ src/classes/interface.rb
@@ -1,10 +1,27 @@
 class Interface
+
 	attr_reader :mac
 	attr_reader :mtu
 	attr_reader :ip
-	def initialize(mac, ip, mtu)
+	attr_reader :name
+
+	def initialize(mac, ip, mtu, name)
 		@mac = mac
 		@ip  = ip
 		@mtu = mtu
+		@name = name
 	end
+		
+	def arp_request
+	end
+		
+	def arp_reply
+	end
+
+	def icmp_request
+	end
+
+	def icmp_reply
+	end
+
 end
