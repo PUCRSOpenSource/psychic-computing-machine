9ddad6870524e85bfccec5883cf4db501ec81dae
Matthias Nunes <matthiasnunes@gmail.com>
2016-06-18 03:11:35 -0300

    Removes useless stuff from previous model

--- src/classes/network.rb
+++ src/classes/network.rb
@@ -1,29 +1,11 @@
 class Network
+
 	attr_reader :address
-	attr_reader :nodes
-	attr_reader :router
+	attr_reader :interfaces
+
 	def initialize address
-		@address = address
-		@nodes = {}
+		@address    = address
+		@interfaces = {}
 	end
 
-	def add_node node_name, interface
-		@nodes[node_name] = interface
-	end
-
-	def broadcast package
-
-	end
-
-	def search_node_by_name
-
-	end
-
-	def search_node_by_ip
-
-	end
-
-	def search_node_by_mac
-
-	end
 end
