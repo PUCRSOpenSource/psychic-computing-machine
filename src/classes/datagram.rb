b506aa9b58ff48eb7395e2268c7d9125e1d2457d
Diego Pinto da Jornada <diego.p.jornada@gmail.com>
2016-06-19 01:17:41 -0300

    updates datagram

--- src/classes/datagram.rb
+++ src/classes/datagram.rb
@@ -4,16 +4,18 @@ class Datagram
 	attr_reader :ip_dst
 	attr_reader :name_src
 	attr_reader :name_dst
-	attr_reader :msg
+	attr_reader :message
 	attr_reader :datagram
+	attr_reader :ttl
 
-	def initialize ip_src, ip_dst, name_src, name_dst, msg=nil, datagram=nil
+	def initialize ip_src, ip_dst, name_src, name_dst, message=nil, datagram=nil
 		@ip_src = ip_src
 		@ip_dst = ip_dst
 		@name_src = name_src
 		@name_dst = name_dst
-		@msg = msg
+		@message = msg
 		@datagram = datagram
+		@ttl = 8
 	end
 
 end
