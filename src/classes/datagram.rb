0d023da9e989d0629144a6eefb4118b66e7ad9bd
Matthias Nunes <matthiasnunes@gmail.com>
2016-06-19 01:13:33 -0300

    Adds more stuff to datagram

--- src/classes/datagram.rb
+++ src/classes/datagram.rb
@@ -2,12 +2,16 @@ class Datagram
 
 	attr_reader :ip_src
 	attr_reader :ip_dst
+	attr_reader :name_src
+	attr_reader :name_dst
 	attr_reader :msg
 	attr_reader :datagram
 
-	def initialize ip_src, ip_dst, msg=nil, datagram=nil
+	def initialize ip_src, ip_dst, name_src, name_dst, msg=nil, datagram=nil
 		@ip_src = ip_src
 		@ip_dst = ip_dst
+		@name_src = name_src
+		@name_dst = name_dst
 		@msg = msg
 		@datagram = datagram
 	end
