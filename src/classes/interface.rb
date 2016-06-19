f654dd74c71222b488ac6942eeb85ddc7ed79a85
Matthias Nunes <matthiasnunes@gmail.com>
2016-06-19 16:06:18 -0300

    Move node logic out of the interface icmp_reply

--- src/classes/interface.rb
+++ src/classes/interface.rb
@@ -33,47 +33,13 @@ class Interface
 	end
 
 	def icmp_request datagram
-		#MATTHIAS: print do professor mostra diferente o IP no primeiro ICMP
-		#          request.
 		puts "#{@name} => #{datagram.name_dst} : ICMP - Echo (ping) request (src=#{datagram.ip_src} dst=#{datagram.ip_dst} ttl=#{datagram.ttl} data=#{datagram.get_message});"
 		@network.icmp_request datagram
 	end
 
 	def icmp_reply datagram
-		#binding.pry
-		next_datagram = nil
-		if datagram.datagram.nil?
-			next_datagram = Datagram.new datagram.ip_dst, datagram.ip_src, datagram.name_dst, datagram.name_src, datagram.message
-			next_datagram.reply = true
-			icmp_echo datagram.get_message
-			unless datagram.reply
-				puts "#{@name} => #{datagram.name_src} : ICMP - Echo (ping) reply (src=#{datagram.ip_dst} dst=#{datagram.ip_src} ttl=#{datagram.ttl} data=#{datagram.message});"
-				@network.icmp_reply next_datagram
-			end
-		else
-			if datagram.datagram.ip_dst == @ip
-				next_datagram = Datagram.new datagram.datagram.ip_dst, datagram.datagram.ip_src, datagram.datagram.name_dst, datagram.datagram.name_src, datagram.datagram.message
-				next_datagram.reply = true
-				icmp_echo datagram.datagram.get_message
-				unless datagram.reply
-					puts "#{@name} => #{datagram.name_src} : ICMP - Echo (ping) reply (src=#{datagram.ip_dst} dst=#{datagram.ip_src} ttl=#{datagram.ttl} data=#{datagram.message});"
-					@network.icmp_reply next_datagram
-				end
-			else
-				router = @network.routers[@name]
-				router.route datagram
-			end
-		end
-		#MATTHIAS: o problema realmente era de quando voltava da stack no
-		#          primeiro roteador, daí ele entrava no unless aqui e
-		#          chamava o reply pra algo q nunca foi definido, por isso
-		#          coloquei o unless repetido ali nos ifs sei que ta uma
-		#          bosta, mas... quero fazer rodar logo, depois se sobrar
-		#          tempo a gente arruma.
-		#unless datagram.reply
-			#puts "#{@name} => #{datagram.name_src} : ICMP - Echo (ping) reply (src=#{datagram.ip_dst} dst=#{datagram.ip_src} ttl=#{datagram.ttl} data=#{datagram.message});"
-			#@network.icmp_reply next_datagram
-		#end
+		router = @network.routers[@name]
+		router.route datagram
 		return
 	end
 
