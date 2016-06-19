6d903ba494d640a5fe9ee49c31ba9becb8bd5448
Matthias Nunes <matthiasnunes@gmail.com>
2016-06-19 06:09:55 -0300

    passing the ball to diego

--- src/classes/interface.rb
+++ src/classes/interface.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Interface
 
 	attr_reader :mac
@@ -31,24 +33,47 @@ class Interface
 	end
 
 	def icmp_request datagram
-		puts "#{@name} => #{datagram.name_dst} : ICMP - Echo (ping) request (src=#{@ip} dst=#{datagram.ip_dst} ttl=#{datagram.ttl} data=#{datagram.get_message});"
+		#MATTHIAS: print do professor mostra diferente o IP no primeiro ICMP
+		#          request.
+		puts "#{@name} => #{datagram.name_dst} : ICMP - Echo (ping) request (src=#{datagram.ip_src} dst=#{datagram.ip_dst} ttl=#{datagram.ttl} data=#{datagram.get_message});"
 		@network.icmp_request datagram
 	end
 
 	def icmp_reply datagram
+		#binding.pry
 		next_datagram = nil
-		if datagram.datagram.nil? || datagram.datagram.ip_dst == @ip
+		if datagram.datagram.nil?
 			next_datagram = Datagram.new datagram.ip_dst, datagram.ip_src, datagram.name_dst, datagram.name_src, datagram.message
 			next_datagram.reply = true
 			icmp_echo datagram.get_message
+			unless datagram.reply
+				puts "#{@name} => #{datagram.name_src} : ICMP - Echo (ping) reply (src=#{datagram.ip_dst} dst=#{datagram.ip_src} ttl=#{datagram.ttl} data=#{datagram.message});"
+				@network.icmp_reply next_datagram
+			end
 		else
-			router = @network.routers[@name]
-			router.route datagram
+			if datagram.datagram.ip_dst == @ip
+				next_datagram = Datagram.new datagram.datagram.ip_dst, datagram.datagram.ip_src, datagram.datagram.name_dst, datagram.datagram.name_src, datagram.datagram.message
+				next_datagram.reply = true
+				icmp_echo datagram.datagram.get_message
+				unless datagram.reply
+					puts "#{@name} => #{datagram.name_src} : ICMP - Echo (ping) reply (src=#{datagram.ip_dst} dst=#{datagram.ip_src} ttl=#{datagram.ttl} data=#{datagram.message});"
+					@network.icmp_reply next_datagram
+				end
+			else
+				router = @network.routers[@name]
+				router.route datagram
+			end
 		end
-		unless datagram.reply
-			puts "#{@name} => #{datagram.name_src} : ICMP - Echo (ping) reply (src=#{@ip} dst=#{datagram.ip_src} ttl=#{datagram.ttl} data=#{datagram.message});"
-			@network.icmp_reply next_datagram
-		end
+		#MATTHIAS: o problema realmente era de quando voltava da stack no
+		#          primeiro roteador, daí ele entrava no unless aqui e
+		#          chamava o reply pra algo q nunca foi definido, por isso
+		#          coloquei o unless repetido ali nos ifs sei que ta uma
+		#          bosta, mas... quero fazer rodar logo, depois se sobrar
+		#          tempo a gente arruma.
+		#unless datagram.reply
+			#puts "#{@name} => #{datagram.name_src} : ICMP - Echo (ping) reply (src=#{datagram.ip_dst} dst=#{datagram.ip_src} ttl=#{datagram.ttl} data=#{datagram.message});"
+			#@network.icmp_reply next_datagram
+		#end
 		return
 	end
 
@@ -57,7 +82,7 @@ class Interface
 	end
 
 	def send_message datagram
-		next_datagram = Datagram.new @ip, datagram.datagram.ip_dst, @name, datagram.datagram.name_dst, nil, datagram.datagram
+		next_datagram = Datagram.new datagram.ip_src, datagram.datagram.ip_dst, @name, datagram.datagram.name_dst, nil, datagram.datagram
 		arp_request next_datagram.ip_dst
 		icmp_request next_datagram
 	end
